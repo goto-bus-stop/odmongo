fa300ca769c87bd27ee964cea14e02da65e86d91
Renée Kooi <renee@kooi.me>
2020-08-08 17:34:14 +0200

    Export Query and Aggregate

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 exports.Connection = require('./src/Connection.js')
 exports.Model = require('./src/Model.js')
+exports.Query = require('./src/Query.js')
+exports.Aggregate = require('./src/Aggregate.js')
--- index.mjs
+++ index.mjs
@@ -1,4 +1,6 @@
 import Connection from 'odmongo/Connection'
 import Model from 'odmongo/Model'
+import Query from 'odmongo/Query'
+import Aggregate from 'odmongo/Aggregate'
 
-export { Connection, Model }
+export { Connection, Model, Query, Aggregate }
